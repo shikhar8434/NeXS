4afcfd97c206e05a14f0915a0de4ff25c602e2f9
Mayank Dwivedi <mayankd20062001@gmail.com>
2020-11-09 23:42:52 +0530

    built the navigation drawer, the search UI, main activity layout.

--- NExS/app/src/main/java/com/example/nexs/MainActivity.java
+++ NExS/app/src/main/java/com/example/nexs/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.nexs;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar mainToolBar;
+    DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainToolBar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(mainToolBar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.main_menu_search : onSearchRequested();
+                return true;
+            case R.id.home : drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            default :
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
